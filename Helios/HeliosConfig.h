677bed031de4e56bca76df3f773c6bd87a6ff851
Kurt LaVacque <lavacquek@icloud.com>
2025-02-14 20:35:59 +0100

    Simplify patterns and remove unused pattern variants

--- Helios/HeliosConfig.h
+++ Helios/HeliosConfig.h
@@ -67,12 +67,12 @@
 // Max Color Slots
 //
 // The number of slots in a colorset
-#define NUM_COLOR_SLOTS 6
+#define NUM_COLOR_SLOTS 3
 
 // Mode Slots
 //
 // The number of modes on the device
-#define NUM_MODE_SLOTS 6
+#define NUM_MODE_SLOTS 3
 
 // Number of Global Brightness Options
 //
