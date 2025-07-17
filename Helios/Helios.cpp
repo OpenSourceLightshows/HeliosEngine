f9d1145b4243ad61f0970163a73649ef03948454
Kurt LaVacque <lavacquek@icloud.com>
2025-07-17 11:13:54 +0200

    Clarify factory reset condition: update comment to specify that flags are checked for both uninitialized (0xff) and invalid states

--- Helios/Helios.cpp
+++ Helios/Helios.cpp
@@ -207,7 +207,7 @@ void Helios::load_global_flags()
 {
   // read the global flags from index 0 config
   global_flags = (Flags)Storage::read_global_flags();
-  // if the flags are uninitialized (aka 0xff) then run factory reset
+  // if flags are uninitialized (0xff) or invalid then run factory reset
   if (global_flags & FLAGS_INVALID) {
     factory_reset();
     return;
