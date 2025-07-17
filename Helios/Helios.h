a7e6091423f1bc5592fbc4ee6f8a5c7196499b74
Kurt LaVacque <lavacquek@icloud.com>
2025-07-17 10:59:59 +0200

    Refactor global flags definition: remove binary comments for clarity and maintain consistency

--- Helios/Helios.h
+++ Helios/Helios.h
@@ -30,11 +30,11 @@ public:
 
   enum Flags : uint8_t {
     FLAG_NONE      = 0,
-    FLAG_LOCKED    = (1 << 0),   // 0b00000001
-    FLAG_CONJURE   = (1 << 1),   // 0b00000010
-    FLAG_AUTOPLAY  = (1 << 2),   // 0b00000100
+    FLAG_LOCKED    = (1 << 0),
+    FLAG_CONJURE   = (1 << 1),
+    FLAG_AUTOPLAY  = (1 << 2),
 
-    FLAG_END,                    // 5 (auto-increment: count of flags)
+    FLAG_END,
     FLAG_INVALID   = (uint8_t)(~((1 << (FLAG_END - 1)) - 1))
   };
 
